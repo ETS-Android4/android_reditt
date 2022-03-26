8717e666343af26c7cfb215d126cebc7226b6416
ilyassfilali <ilyassfilali6@gmail.com>
2022-03-26 09:13:41 +0100

    new Post

--- app/src/main/java/fr/uge/myapplication/ui/newPost.java
+++ app/src/main/java/fr/uge/myapplication/ui/newPost.java
@@ -2,6 +2,7 @@ package fr.uge.myapplication.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,12 +16,14 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import fr.uge.myapplication.R;
+import fr.uge.myapplication.service.AuthService;
 import fr.uge.myapplication.service.network.Httpservice;
 
 public class newPost extends AppCompatActivity {
 
     TextInputEditText title;
     TextInputEditText content;
+    Httpservice http;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,33 +31,62 @@ public class newPost extends AppCompatActivity {
         setContentView(R.layout.activity_new_post);
         title = findViewById(R.id.newtitlePost);
         content = findViewById(R.id.newcontentpost);
-
+        http =Httpservice.getinstance();
+        http.setCtx(newPost.this);
+        //login();
         Button b = findViewById(R.id.createPost);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 JSONObject js = new JSONObject();
+                final boolean[] good = {false};
                 try {
                     js.put("title",title.getEditableText().toString());
                     js.put("content",content.getEditableText().toString());
-                    Httpservice.getinstance().postRequest("http://192.168.1.114:8080/user/createPost", js, new Response.Listener<String>() {
+                    System.out.println(http.getCokkie());
+                    http.postRequest("http://192.168.1.114:8080/user/createPost", js, new Response.Listener<String>() {
                         @Override
                         public void onResponse(String response) {
                             if(response.split(",")[0].equals("200")){
-
+                                good[0] = true;
                                 newPost.this.finish();
                             }
                         }
                     }, new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
-
+                            Toast.makeText(newPost.this,"can't creat post",Toast.LENGTH_LONG).show();
                         }
                     });
                 }catch (Exception e){
                 }
-                Toast.makeText(newPost.this,"can't creat post",Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    void login(){
+        AuthService auth = new AuthService(http);
+        auth.login("Ilyass", "test789", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(response.split(",")[0].equals("200")){
+                    System.out.println(response.split(",").length);
+                    System.out.println(response.split(",")[0]);
+                    try {
+
+                        System.out.println(response.split(",")[2]);
+                        http.setCokkie(response.split(",")[2]);
+                    }catch (Exception e){
+
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        System.out.println("connected");
     }
 }
